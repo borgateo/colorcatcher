fb3a6af74f3f6234b723c03588a1e04f0d67598e
Borteo <mattborgato@invisionapp.com>
2016-05-10 01:19:18 +0100

    update: move to one raw, remove guide, add scroll style

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 600, height: 600});
+  mainWindow = new BrowserWindow({width: 640, height: 320, resizable: false});
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
